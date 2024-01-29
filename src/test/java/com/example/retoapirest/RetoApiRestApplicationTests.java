d8aba771166183cb9fde809add8d6e542f396996
aphelio22 <jrgaln.m@gmail.com>
2024-01-29 16:59:47 +0100

    Initial commit

---
+++ src/test/java/com/example/retoapirest/RetoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
